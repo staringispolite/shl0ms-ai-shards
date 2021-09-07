0d61e1324240e47897779e8c235f77e2cf992c28
Jonathan Howard <howard.jonathan@gmail.com>
2021-09-06 19:01:20 -0700

    config updates

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -31,7 +31,7 @@ const config: HardhatUserConfig = {
     purchaser: 0,
   },
   solidity: {
-    version: "0.7.0",
+    version: "0.7.6",
     settings: {
       optimizer: {
         enabled: true,
